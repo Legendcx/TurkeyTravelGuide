43284e1498c7948f8f2d7b9ec2d0f591e9882481
burhan yiğiter <burhan.yigiter@gmail.com>
2022-10-13 17:29:48 +0300

    TourProject

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,11 @@ import "./Navbar.scss";
 const Navbar = () => {
     return(
         <div className="Navbar">
-            <a href="#aboutus">About Us</a>
+            <a href="#home">Home</a>
             <a href="#foryou">For You</a>
             <a href="#services">Services</a>
             <a href="#blog">Blog</a>
-            <a href="#vlog">Vlog</a>
+            <a href="#aboutus">About Us</a>
             <a href="#contact">Contact</a>
             
         </div>
---
+++ src/helper/data.js
@@ -0,0 +1,127 @@
+export const data = [
+    {
+      id: 1,
+      // title: "New York",
+      title: "Sivas/Divriği Ulu Camii",
+      desc:
+      " Mimari özelliklerinin yanı sıra, sergilediği zengin Anadolu geleneksel taş işçiliği örnekleriyle 1985 yılından itibaren UNESCO Dünya Miras Listesi'nde yer almaktadır. mengücek Beyliği döneminde 1228-1243 yılları aasında inşa edildi.K âinattaki farklı varlıkların muhteşem bir ahenk ve denge içerisinde olduklarının taşa nakşedilerek gözler önüne serilmiştir. Evliya Çelebi yüzyıllar önce şöyle ifade etmiştir: 'Methinde diller kısır, kalem kırıktır.'",
+        // "Home to the Statue of Liberty, Empire State Building, Central Park, Times Square, New York is the most populous city in the USA. We have a lot to tell about the city where city lights illuminate the sky.",
+      image:
+        // "https://cdn.pixabay.com/photo/2017/06/07/15/47/new-york-city-2380683_960_720.jpg",
+        "https://www.howturkey.com/images/sivas_divrigi_dunya_mirasi_unesco_2.jpg",
+    },
+    {
+      id: 2,
+      // title: "Philippines",
+      title: "İstanbul/Ayasofya Camii",
+      desc:
+      "Ayasofya Camii, İstanbul’daki tarihi yapılar arasında günümüze ulaşan en eski ve görkemli eser. Yapıldığı dönemde Doğu Roma(Bizans) İmparatorluğu’nun en muhteşem katedral binası olan yapı, İstanbul’un fethinden sonrada önemini devam ettirmiş. Camiye çevrilmesi sonrasında ilave edilen İslami eserler ile daha bir zengin hale gelen yapı, devasa kubbesi altındaki geniş mekanı ve mimari güzelliği her yıl milyonlarca ziyaretçiyi ağırlamakta.",
+        // "The island country, the Philippines, is one of the most beautiful countries where blue and green are together. Forget the potion of youth, traveling to the Philippines will rejuvenate you for at least 5 years.",
+      image:
+      "https://static6.depositphotos.com/1006054/620/i/600/depositphotos_6202226-stock-photo-hagia-sophia-interior.jpg",
+        // "https://bigseventravel.com/wp-content/uploads/2019/11/john-hernandez-IL-fBBnF-GU-unsplash-1920x1440.jpg",
+    },
+    {
+      id: 3,
+      // title: "Bali",
+      title: "Edirne/Selimiye Camii",
+      desc:
+        "Dört adet yükselen ince minareleri, muhteşem dekore edilmiş iç mekanı, el yazması kitapların bulunduğu kütüphanesi, titiz işçilik, parlak İznik çinileri ve mermer avlu ile birleşmiş eğitim kurumları, dış avlu ve kapalı çarşı ile birlikte tek büyük kubbesine doğru yükselen heybetli bir camidir. Selimiye Camii ve Külliyesi, hâkim konumdaki mimari kompozisyonu, Osmanlı İmparatorluğunun en seçkin mimarı Sinan'ın büyük eserinin doruk noktasını temsil eder. Bu özelliklerinden dolayı 29 Haziran 2011 yılında UNESCO DÜNYA MİRASI listesine seçilmiştir.",
+        // "Bali, Indonesia's most famous island, has taken its place as the 'last paradise' in mind. Providing 80% of its livelihood with tourism, Bali hosts thousands of tourists every year.",
+      // image: "https://ak.picdn.net/shutterstock/videos/26368511/thumb/1.jpg",
+      image: "https://www.howturkey.com/images/edirne_selimiye_camii_unesco_dunya_mirasi.jpg",
+    },
+    {
+      id: 4,
+      title: "Muğla/ Xantos",
+      // title: "Egypt",
+      desc:
+        "Xanthos, dağlık Likya eyaletinin en eski ve en büyük kentidir. Xanthos ırmağının vadisinde kuruludur. Pers istilasına değin bağımsız yaşamıştır. Pers istilasında kentlerini kahramanca savunan Xanthos'lular, istilayı önleyemeyeceklerini anlayınca önce tüm kadın ve çocuklarını öldürmüşler, sonrada kenti ateşe vererek ve bu alevlerin içine kendilerini atarak topluca intihar etmişlerdir.",
+        // "Being one of the biggest countries of the Middle East, Egypt has been the transition point of many civilizations with its thousands of years of history. The pyramids and miracles that come to mind when talking about Egypt have been the subject of many films. It still remains a mystery.",
+      image:
+        "https://www.howturkey.com/images/antalya_mugla_xanthos_letoon_fethiye_unesco_dunya_mirasi.jpg",
+        // "https://images.memphistours.com/large/34d5b5a3fbaa4b3b5d9487bf924b0145.jpg",
+    },
+    {
+      id: 5,
+      title: "Nevşehir/Cappadocia",
+      desc:
+        "Tamamen erozyonla şekillendirilmiş muhteşem bir manzara içinde, Göreme vadisi ve çevresi, İkonoklastik sonrası dönemde Bizans sanatının benzersiz kanıtı olan kayaya oyulmuş tapınaklar içermektedir. 4. yüzyıla dayanan geleneksel bir insan habitatının kalıntıları olan konutlar, ilkel yaşam köyleri ve yeraltı kasabaları da burada görülebilir.",
+        // "Cappadocia… 'Kappa Tuchia', which means 'Land of Beautiful Horses' in the words of Persians. A location in Turkey and in the center of Anatolia's most popular and unique tourist centers of someone who Cappadocia, magnificent nature, valleys of unique beauty, fairy chimneys, rock-hewn settlements, millions every year with open-air museums and underground cities, local and hosts foreign tourists.",
+      image: "https://i.hurimg.com/i/hdn/75/0x0/5aa10544d3806c25541af273.jpg",
+    },
+    {
+      id: 6,
+      title: "Çorum/Hattuşa",
+      // title: "Paris",
+      desc:
+        "Hitit imparatorluğunun eski başkenti Hattuşa'nın arkeolojik alanı, kentsel organizasyonu, korunan yapı türleri (tapınaklar, saraylar, şehir kalıntıları ), Lions Kapısı ile Kraliyet Kapısı'nın zengin süslemesi Ve Yazılıkaya'daki kaya sanatı topluluğudur. Yapılan kazılarda 5 kültür katı ortaya çıkmıştır. Bu katlarda Hatti, Asur, Hitit, Frig, Galat, Roma ve Bizans dönemlerinden kalma kalıntılar bulunmuştur. Kalıntılar Aşağı Kent, Yukarı Kent, Büyük Kale (Kral Kalesi), Yazılıkaya'dan oluşmaktadır.",
+        // "Paris has been recognized as one of the most romantic cities in the world. Many famous buildings here have been added to the Unesco World Heritage List. It awaits you with its museums, fashion and historical buildings.",
+      image:
+        "https://www.howturkey.com/images/corum_hattusa_hitit_unesco_dunya_mirasi.jpg",
+        // "https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20180221130827/iStock-155381519.jpg",
+    },
+    {
+      id: 7,
+      title: "Adıyaman/Nemrut Dağı",
+      // title: "Venice",
+      desc:
+        "Doğu ve Batı Medeniyetlerinin, 2150 m. yükseklikte muhteşem bir piramitteki kesişme noktası, Dünyanın sekizinci harikası Nemrut, anıtsal heykelleri ve benzersiz manzarası ile Helenistik Dönemin en görkemli kalıntılarından birisidir Yüksekliği on metreyi bulan büyüleyici heykelleri, metrelerce uzunlukta kitabeleri bulunmaktadır. Eski çağlarda ‘‘Kommagene‘‘ olarak bilinen, bu alanda bulunan arkeolojik kalıntılar Antiochos tümülüsü ve buradaki dev heykeller, Eskikale, Yenikale, Karakuş Tepe ve Cendere Köprüsü Nemrut Dağı, çevresindeki önemli eserlerdir.",
+        // "Venice, one of Italy's indispensable stops, is known as the 'City of Channels', 'City of Waters' or 'City of Masks' thanks to its world-famous masks. If you visit in February, you can catch the Venice Carnival, where everyone wears a mask and has fun.",
+      image:
+        "https://www.howturkey.com/images/adiyaman_nemrut_dagi_komagene_dunya_mirasi_unesco_3.jpg",
+        // "https://www.qantas.com/content/travelinsider/en/explore/europe/italy/venice/things-you-need-to-know-before-you-go-to-venice/_jcr_content/parsysTop/hero.img.full.medium.jpg/1535006246068.jpg",
+    },
+    {
+      id: 8,
+      title: "Denizli/Pamukkale",
+      // title: "India",
+      desc:
+        "4. ve 6. yüzyıllar arasında inşa edilen Hierapolis'te heykeller şehir kalıntıları, katedral, hamam ve kiliseler Erken Hıristiyanlık mimari grubunun seçkin bir örneğini teşkil etmektedir. Çal Dağı'nın güney eteklerinden gelen kalsiyum oksit içeren suların oluşturduğu görkemli beyaz travertenler Pamukkale'de görsel bir çarpıcı bir manzara yaratmıştır. Büyük ölçüde bozulmamış ve doğa manzarası korunan beyaz traverten terasları, binlerce antik sütun ve mermer mimari süslemeleri arasında oluşmuş sıcak su havuzları, Hierapolis (Holy City-Kutsal Kent) arkeolojik kentinde, birlikte iç içedir.",
+        // "If you are not a vacationer but a traveler, we can say that India is the place for you. India, one of the most colorful places in the world, has a crazy variety. The Taj Mahal, the story of which you have heard more or less, is one of its famous buildings.",
+      image: "https://www.howturkey.com/images/denizli_pamukkale_hierapolis_dunya_mirasi_unesco_1.jpg",
+      // image: "https://lp-cms-production.imgix.net/2020-11/GettyRF_494057771.jpg",
+    },
+    {
+      id: 9,
+      title: "Çanakkale/Troya Antik Kenti",
+      // title: "Northern Lights",
+      desc:
+        "Troya, dünyadaki en ünlü antik kentlerden birisidir. Troya'da görülen 9 katman, kesintisiz olarak 3000 yıldan fazla bir zamanı göstermekte ve Anadolu, Ege ve Balkanların buluştuğu bu benzersiz coğrafyada yerleşmiş olan uygarlıkları izlememizi sağlamaktadır. Troya'daki en erken yerleşim katı M.Ö. 3000 - 2500 ile erken Tunç Çağı'na tarihlenmektedir, daha sonra sürekli yerleşim gören Troya katmanları M.Ö. 85 - M.S. 8. yüzyıla tarihlenen Roma Dönemi ile sona ermektedir. ",
+        // "Northern lights, the 'Aurora Borealis', named after the Dawn Goddess Aurora, are the natural glows that emerge in the sky with the interaction of the earth's magnetic field and charged particles from the Sun. This image, which makes most of us feel like in a fairy tale land, deserves to be visited at least once in a lifetime.",
+      image:
+        "https://www.howturkey.com/images/canakkale_troya_antik_unesco_dunya_mirasi.jpg",
+        // "https://images.unsplash.com/photo-1579033461380-adb47c3eb938?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bm9ydGhlcm4lMjBsaWdodHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    },
+    {
+      id: 10,
+      title: "İzmir/Efes",
+      desc:
+        " Efes, tarih öncesi dönemden başlayarak Helenistik, Roma, Doğu Roma, Beylikler ve Osmanlı dönemleri boyunca yaklaşık 9000 yıl kesintisiz yerleşim görmüştür. Helenistik ve Roma Döneminin üstün kentleşme, mimarlık ve dini tarihine ışık tutan simgeleri barındıran Efes'te farklı dönemlere ait en üstün mimari ve kent planlama örnekleri bulunmaktadır. M. Ö. 8. yüzyıla tarihlenen ve Antik dönemin yedi harikasından biri olarak ünlenen kült merkezi Artemision, Hz. Meryem'in İsa'nın annesi olarak kabul ve ilan edildiği 431 tarihli Ekümenik Konsülün gerçekleştiği yer olan Meryem Kilisesi, İsa'nın havarilerinden biri olan ve Yahya İncili'ni Efes'te yazan St. John'ın mezarı üzerine inşa edilen Bazilika gibi Erken Hristiyanlık dönemine şahitlik eden benzersiz eserleri, günümüzde Hristiyanlar tarafından hac mekanı olarak kabul edilen Meryem Ana Evi ve Beylikler döneminde inşa edilen İslam yapıları ile Efes aynı zamanda dini tarih açısından da bugün hala ayakta olan benzersiz bir birikim sunmaktadır.",
+        // "The symbol and highest mountain of Japan, Mount Fuji is considered sacred by the Japanese and is called Fuji-San. This mountain, which has an active startovolcano, is depicted in art and photography and is a natural wonder visited by athletes and tourists.",
+      image:
+        "https://www.howturkey.com/images/izmir_efes_antik_kenti_unesco_dunya_mirasi.jpg",
+        // "https://d20aeo683mqd6t.cloudfront.net/articles/title_images/000/038/943/original/pixta_43064884_S.jpg?2019&d=750x400",
+    },
+    {
+      id: 11,
+      title: "Şanlıurfa/Göbeklitepe",
+      // title: "Arashiyama Bamboo Grove",
+      desc:
+        "Mezopotamya’daki en eski tapınak olan Göbeklitepe Arkeolojik Alanı, 1963 yılında, İstanbul ve Chicago Üniversitelerinin ortaklığıyla gerçekleştirilen bir yüzey araştırması sırasında keşfedilmiştir. Alanın gerçek değeri ise; 1994 yılından sonraArkeoloji Enstitüsü ile Şanlıurfa Müzesi'nin ortak çalışması ile yapılan kazı çalışmaları sonucunda, ortaya çıkmaya başlamıştır. Yapılan kazılarda Göbeklitepe’nin, konumu, boyutları, tarihlendirilmesi ve yapılarının anıtsallığı ile Neolitik dönem için kutsal bir alan olduğu anlaşılmıştır. Bu özel alan bir yerleşim yeri değil, kült merkez olarak tanımlanmaktadır. Göbekli Tepe'de ortaya çıkarılan tapınağı bu kadar önemli kılan neden ise; tapınağı yapanların, yerleşik hayata geçmemiş avcı-toplayıcı insanlar olmasıdır. Bu da yerleşik yaşama geçişte ekonomik ya da ekolojik değil, kalabalık ve uzun süreli dinsel törenlerin rol oynamış olabileceğini göstermektedir.",
+        // "Bamboo Forest, or Arashiyama Bamboo Grove or Sagano Bamboo Forest, is a natural forest of bamboo in Arashiyama, Kyoto, Japan. The forest consists mostly of Moso Bamboo (Phyllostachys edulis) and consists of several pathways for tourists and visitors. The Ministry of the Environment considers it a part of the soundscape of Japan.",
+      image: "https://www.howturkey.com/images/urfa_gobeklitepe_kenti_dunya_mirasi_unesco_3.jpg",
+      // image: "https://wallpaperaccess.com/full/534182.jpg",
+    },
+    {
+      id: 12,
+      title: "Antalya/Kleopatra Plajı",
+      // title: "Antelope Canyon",
+      desc:
+        "Alanya’da yer alan uzun bir plaj olmasıyla ünlenen Damlataş Kleopatra Plajı yaz aylarının vazgeçilmez tatil mekanları arasındadır. Bir ucunda Damlataş Mağarası da bulunan plaj; birçok şemsiye ve şezlonguyla göz doldurmaktadır. Kasım ve aralık ayında bile kumsalda çıplak ayakla gezmenin mümkün olduğu bu yer, “mavi bayrak” taşımasıyla da çok büyük önem arz etmektedir",
+        // "Antelope Canyon is a slot canyon in the American Southwest, on Navajo land east of Page, Arizona. It includes two separate, scenic slot canyon sections, referred to as Upper Antelope Canyon (or The Crack), and Lower Antelope Canyon (or The Corkscrew).",
+      image:
+        "https://www.yaz-tatili.com/wp-content/uploads/kleopatra-plaji.jpg",
+        // "https://images.unsplash.com/photo-1444076784383-69ff7bae1b0a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGxvd2VyJTIwYW50ZWxvcGUlMjBjYW55b258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    },
+  ];
